0a21aadf1862b725fb9067031c6568fe842d542a
GitHubDavidM <158535145+GitHubDavidM@users.noreply.github.com>
2024-02-09 11:15:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert('Bonjour le monde !');
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+var task = "task";
+var i = 0;
+addTaskBtn.addEventListener("click",addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if(taskText !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Supprimer";
+
+    listItem.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click",() => {
+        listItem.remove();
+    });
+    } else {
+    alert("Veuillez entrer une tâche valide.");
+    }
+localStorage.setItem(task.concat(i), taskText);
+i++;
+}
